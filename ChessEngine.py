3bd8f06d91e9b4a2cba5c0c848ff02d353ff541a
lth1013 <lukeharriman1013@gmail.com>
2024-07-20 13:10:02 -0700

    GameState class and board

---
+++ ChessEngine.py
@@ -0,0 +1,6 @@
+"""
+This class is responsible for storing all the information about the current state of a chess game.
+It is also responsible for determining the valid moves at the current state.
+It will also keep a move log.
+"""
+
---
+++ ChessMain.py
@@ -0,0 +1,17 @@
+"""
+Main driver file. 
+This will handle user input and display the current GameState object.
+"""
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
